f9360fa14643a16b80c5aca72757d84b35e5e57b
aspenolmsted <88668915+aspenolmsted@users.noreply.github.com>
2023-05-01 10:39:44 -0400

    Add files via upload

---
+++ ProjectPartI/ASPENTCalc.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="ASPENTCalc.aspx.vb" Inherits="ASPENTCalc.ASPENTCalc" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+     <form id="form1" runat="server">
+        <asp:TextBox id="n1" runat="server"/>
+        <asp:TextBox id="n2" runat="server"/>
+        <asp:DropDownList id="oper" runat="server">
+            <asp:ListItem Text="+" Value="+" />
+            <asp:ListItem Text="-" Value="-" />
+            <asp:ListItem Text="*" Value="*" />
+            <asp:ListItem Text="/" Value="/" />
+
+        </asp:DropDownList>
+        <asp:Button text="Submit" runat="server"/>
+    </form>
+</body>
+</html>
---
+++ ProjectPartI/ASPENTCalc.aspx.vb
@@ -0,0 +1,31 @@
+﻿Public Class ASPENTCalc
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim n1, n2, oper As String
+        Dim res As String = ""
+        n1 = Request.Form("n1")
+        n2 = Request.Form("n2")
+        oper = Request.Form("oper")
+        If Not n1 Is Nothing Then
+            Select Case oper
+                Case "+"
+                    res = CInt(n1) + CInt(n2)
+                Case "-"
+                    res = n1 - n2
+                Case "*"
+                    res = n1 * n2
+                Case "/"
+                    If n2 = 0 Then
+                        Response.Write("Cannot divide by zero!")
+                    Else
+                        res = n1 / n2
+                    End If
+            End Select
+            If Len(n1) > 0 Then
+                Response.Write("The result is: " & res)
+            End If
+        End If
+    End Sub
+
+End Class
