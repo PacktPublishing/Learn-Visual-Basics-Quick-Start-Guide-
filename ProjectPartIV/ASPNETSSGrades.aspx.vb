772775e1ef60aa2631a2939789ea06649c6badd3
aolmsted <39239480+aolmsted@users.noreply.github.com>
2023-08-24 15:30:06 -0400

    Added project part iv

---
+++ ProjectPartIV/ASPNETSSGrades.aspx.vb
@@ -0,0 +1,13 @@
+﻿Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Session("Total") += CInt(tbGrade.Text)
+        Session("Cnt") += 1
+        lblAverage.Text = "Grade Average: " & Session("Total") / Session("Cnt")
+    End Sub
+End Class
